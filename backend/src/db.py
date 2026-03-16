be2fd221d7d8a25aaadcc811243d86aa15a0b698
Kamil Kluska <kamil6kluska7@gmail.com>
2026-03-16 17:33:26 +0100

    Update: Session

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,11 +1,17 @@
 import os
+
 from dotenv import load_dotenv
+from typing import Annotated
 
-from sqlalchemy.ext.asyncio import AsyncEngine
+from fastapi import Depends
 
-from src.models import Base
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
 
-def get_db_url() -> str:
+def get_db_url(asynchronous=False) -> str:
     load_dotenv()
 
     user = os.environ['POSTGRES_USER']
@@ -13,9 +19,19 @@ def get_db_url() -> str:
     host = os.environ['POSTGRES_HOST']
     port = os.environ['POSTGRES_PORT']
     db = os.environ['POSTGRES_DB']
-    
-    return f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db}"
 
-async def init_db(a_engine: AsyncEngine):
-    async with a_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    if asynchronous:
+        return f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db}"
+    else:
+        return f"postgresql://{user}:{password}@{host}:{port}/{db}"
+
+
+a_engine = create_async_engine(get_db_url(asynchronous=True), echo=True)
+a_sessionmaker = async_sessionmaker(a_engine, expire_on_commit=False)
+
+async def get_session():
+    async with a_sessionmaker() as session:
+        yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
