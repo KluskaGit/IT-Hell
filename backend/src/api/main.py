c2c472c773ea56ebfabcc774e00bedde8294a552
Kamil Kluska <kamil6kluska7@gmail.com>
2026-03-16 17:34:08 +0100

    Update: Removed db init

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,36 +1,6 @@
-from typing import Annotated
+from fastapi import FastAPI
 
-from contextlib import asynccontextmanager
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.ext.asyncio import (
-    create_async_engine,
-    async_sessionmaker,
-    AsyncSession,
-)
-
-from src.db import get_db_url, init_db
-a_engine = create_async_engine(get_db_url())
-a_sessionmaker = async_sessionmaker(a_engine, expire_on_commit=False)
-
-async def get_session():
-    async with a_sessionmaker() as session:
-        yield session
-
-
-SessionDep = Annotated[AsyncSession, Depends(get_session)]
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Before startup
-    await init_db(a_engine=a_engine)
-    yield
-    # After startup
-
-app = FastAPI(lifespan=lifespan)
-
-
+app = FastAPI()
 
 @app.get("/")
 async def root():
