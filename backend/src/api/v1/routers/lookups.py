42d37ea4ea8df46fddfea1855ada84877b4826c2
Kamil Kluska <kamil6kluska7@gmail.com>
2026-03-25 00:16:41 +0100

    feat: Add GET for experience level

---
+++ backend/src/api/v1/routers/lookups.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from typing import List
+
+from src.db import SessionDep
+from src.repositories.lookups import get_experience_levels
+from src.schemas.lookups import (
+    ExperienceLevelRead,
+)
+
+router = APIRouter(tags=["Lookup Data"])
+
+@router.get("/experience-levels", response_model=List[ExperienceLevelRead])
+async def read_experience_levels(session: SessionDep):
+    exp_levels = await get_experience_levels(session)
+    return exp_levels
