9aaa3db76bc547558c834db4d7e80d8d4c82fb2d
Marek Gozdalski <gozdalski.szczepan@gmail.com>
2026-03-26 19:20:37 +0100

    feat: Add GET for company name

--- backend/src/api/v1/routers/lookups.py
+++ backend/src/api/v1/routers/lookups.py
@@ -4,6 +4,7 @@ from typing import List
 from src.db import SessionDep
 from src.repositories import lookups
 from src.schemas.lookups import (
+    CompanyNameRead,
     ExperienceLevelRead,
     SiteUrlRead,
     WorkTypeRead
@@ -25,3 +26,8 @@ async def read_work_types(session: SessionDep):
 async def read_site_urls(session: SessionDep):
     sites_urls = await lookups.get_site_urls(session)
     return sites_urls
+
+@router.get("/company-names", response_model=List[CompanyNameRead])
+async def read_company_names(session: SessionDep):
+    company_names = await lookups.get_company_names(session)
+    return company_names
