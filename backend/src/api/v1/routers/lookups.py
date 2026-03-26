bb7f2d4eb1f255fcbe734937aefdbf3a7cc82f64
Marek Gozdalski <gozdalski.szczepan@gmail.com>
2026-03-26 18:38:46 +0100

    feat: Add GET for work type

--- backend/src/api/v1/routers/lookups.py
+++ backend/src/api/v1/routers/lookups.py
@@ -2,14 +2,20 @@ from fastapi import APIRouter
 from typing import List
 
 from src.db import SessionDep
-from src.repositories.lookups import get_experience_levels
+from src.repositories import lookups
 from src.schemas.lookups import (
     ExperienceLevelRead,
+    WorkTypeRead
 )
 
 router = APIRouter(tags=["Lookup Data"])
 
 @router.get("/experience-levels", response_model=List[ExperienceLevelRead])
 async def read_experience_levels(session: SessionDep):
-    exp_levels = await get_experience_levels(session)
+    exp_levels = await lookups.get_experience_levels(session)
     return exp_levels
+
+@router.get("/work-types", response_model=List[WorkTypeRead])
+async def read_work_types(session: SessionDep):
+    work_types = await  lookups.get_work_types(session)
+    return work_types
