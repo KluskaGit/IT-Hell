b6173687684f02bebea48be0c1a4451fd12700e6
Kamil Kluska <kamil6kluska7@gmail.com>
2026-04-02 15:56:51 +0200

    feat: Create user and get user

---
+++ backend/src/repositories/users.py
@@ -0,0 +1,31 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import select
+
+from src.models.users import (
+    User,
+    UserProfile,
+)
+
+class UserRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_user_by_email(self, email: str) -> User | None:
+        stmt = select(User).where(User.email == email)
+        result = await self.session.execute(stmt)
+        user = result.scalar_one_or_none()
+        
+        return user
+
+    async def create_user(self, email: str, password: str) -> User:
+        user = User(email=email, password=password)
+        self.session.add(user)
+        try:
+            await self.session.commit()
+            await self.session.refresh(user)
+        except IntegrityError:
+            await self.session.rollback()
+            raise ValueError("User with this email already exists.")
+        
+        return user
