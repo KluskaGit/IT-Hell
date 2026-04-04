59c77c7ae1103aef6b686740576f3359ede96a1e
KluskaGit <kamil6kluska7@gmail.com>
2026-04-05 00:00:03 +0200

    feat: Add dependencies for authentication

---
+++ backend/src/api/v1/deps.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends
+
+from src.db import SessionDep
+from src.repositories.users import UserRepository
+from src.services.auth_service import AuthService
+from src.models.users import User
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/auth/login")
+
+def get_user_repo(session: SessionDep) -> UserRepository:
+    return UserRepository(session)
+
+def get_auth_service(user_repo: UserRepository = Depends(get_user_repo)) -> AuthService:
+    return AuthService(user_repo)
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    auth_service: AuthService = Depends(get_auth_service),
+) -> User:
+    
+    return await auth_service.validate_token(token)
