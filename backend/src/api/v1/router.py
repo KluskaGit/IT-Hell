49674e25fe65e22df4ad124e1ea23b2179a4f212
KluskaGit <kamil6kluska7@gmail.com>
2026-04-05 00:01:39 +0200

    feat: Add users and auth routers

--- backend/src/api/v1/router.py
+++ backend/src/api/v1/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from src.api.v1.routers import lookups
+from src.api.v1.routers import lookups, auth, users
 
 router_v1 = APIRouter(prefix="/v1")
 router_v1.include_router(lookups.router)
+router_v1.include_router(auth.router)
+router_v1.include_router(users.router)
 
 
