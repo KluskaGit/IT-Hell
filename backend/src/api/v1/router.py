b4de48bc3c8922548feb4fe0324fb15f93abb24b
KluskaGit <kamil6kluska7@gmail.com>
2026-04-10 23:06:53 +0200

    refactor: remove depracted JWT auth

--- backend/src/api/v1/router.py
+++ backend/src/api/v1/router.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from src.api.v1.routers import lookups, auth, users
+from src.api.v1.routers import lookups, users
 
 router_v1 = APIRouter(prefix="/v1")
 router_v1.include_router(lookups.router)
-router_v1.include_router(auth.router)
 router_v1.include_router(users.router)
 
 
