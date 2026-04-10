bada6afe4385de1c540c046c54274555b0e03352
KluskaGit <kamil6kluska7@gmail.com>
2026-04-10 23:03:24 +0200

    refactor: Add KeyCloak dep

--- backend/src/api/v1/deps.py
+++ backend/src/api/v1/deps.py
@@ -1,15 +1,15 @@
 from typing import Annotated
 
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi import Depends
 
 from src.core.db import SessionDep
+from src.models.users import User
 from src.repositories.users import UserRepository
 from src.services.auth_service import AuthService
-from src.models.users import User
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/auth/login")
+security_scheme = HTTPBearer()
 
 def get_user_repo(session: SessionDep) -> UserRepository:
     return UserRepository(session)
@@ -18,8 +18,8 @@ def get_auth_service(user_repo: UserRepository = Depends(get_user_repo)) -> Auth
     return AuthService(user_repo)
 
 async def get_current_user(
-    token: Annotated[str, Depends(oauth2_scheme)],
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(security_scheme)],
     auth_service: AuthService = Depends(get_auth_service),
 ) -> User:
     
-    return await auth_service.validate_token(token)
+    return await auth_service.authorize(credentials)
