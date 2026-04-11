668ad20418708987a156f332b0c7884fb89c2386
KluskaGit <kamil6kluska7@gmail.com>
2026-04-11 13:05:26 +0200

    fix: Add user_profile_technology junction table

--- backend/src/models/users.py
+++ backend/src/models/users.py
@@ -1,16 +1,32 @@
 import uuid
 
-from typing import Dict, TYPE_CHECKING
+from typing import List, TYPE_CHECKING
 
-from sqlalchemy import ForeignKey, String, Text, Boolean, Uuid, JSON
+from sqlalchemy import (
+    ForeignKey,
+    String,
+    Text,
+    Boolean,
+    Uuid,
+    Table,
+    Column,
+)
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models import Base, TimestampMixin
 
 if TYPE_CHECKING:
-    from src.models.lookups import ExperienceLevel
+    from src.models.lookups import ExperienceLevel, Technology
 
+
+
+user_profile_technologies_table = Table(
+    "user_profile_technology",
+    Base.metadata,
+    Column("user_profile_id", ForeignKey("user_profiles.id", ondelete="CASCADE"), primary_key=True),
+    Column("technology_id", ForeignKey("technologies.id", ondelete="CASCADE"), primary_key=True),
+)
 
 class User(Base, TimestampMixin):
     __tablename__ = "users"
@@ -40,11 +56,17 @@ class UserProfile(Base):
         primary_key=True,
         default=uuid.uuid4
     )
-    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id_keycloak"))
-    exp_level_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("experience_levels.id"))
-    skills: Mapped[Dict] = mapped_column(JSON)
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id_keycloak", ondelete="CASCADE"),)
+    exp_level_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("experience_levels.id", ondelete="CASCADE"))
     raw_cv: Mapped[str | None] = mapped_column(Text, nullable=True)
 
     # Relationships
     user: Mapped["User"] = relationship(back_populates="user_profile")
     exp_level: Mapped["ExperienceLevel"] = relationship(back_populates="user_profiles")
+
+    technologies: Mapped[List["Technology"]] = relationship(
+        secondary="user_profile_technology",
+        back_populates="user_profiles"
+    )
+
+
