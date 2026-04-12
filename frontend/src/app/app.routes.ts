4791b5f7938c2fb04d6fcdb349c3700011282424
Ra1eB <piotrek.ba@op.pl>
2026-04-12 15:41:13 +0200

    added routes and some mock data

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from '../features/auth/login/login.component';
 import { RegisterComponent } from '../features/auth/register/register.component';
 import { ForgotPasswordComponent } from '../features/auth/forgot-password/forgot-password.component';
 import { ProfileComponent } from '../features/profile/profile.component';
+import { OffersComponent } from '../features/offers/offers.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+  { path: 'offers', component: OffersComponent }
 ];
