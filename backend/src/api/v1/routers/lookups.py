c0ab0fc0299b08ef191a5321b0cf99f79530af1a
KluskaGit <kamil6kluska7@gmail.com>
2026-04-12 13:04:22 +0200

    feat: Add example lookups endpoints

--- backend/src/api/v1/routers/lookups.py
+++ backend/src/api/v1/routers/lookups.py
@@ -1,15 +1,26 @@
-from fastapi import APIRouter
-from typing import List
+from typing import List, Annotated
 
-from src.core.db import SessionDep
-from src.repositories.lookups import get_experience_levels
+from fastapi import APIRouter, Depends
+
+from src.services.lookups_service import LookupsService
+from src.api.v1.deps import get_lookups_service
+
 from src.schemas.lookups import (
-    ExperienceLevelRead,
+    LookupRead,
+)
+from src.models.lookups import (
+    ExperienceLevel,
+    WorkType,
 )
 
 router = APIRouter(tags=["Lookup Data"])
 
-@router.get("/experience-levels", response_model=List[ExperienceLevelRead])
-async def read_experience_levels(session: SessionDep):
-    exp_levels = await get_experience_levels(session)
+@router.get("/experience-levels", response_model=List[LookupRead])
+async def get_experience_levels(lookups_service: Annotated[LookupsService, Depends(get_lookups_service)]):
+    exp_levels = await lookups_service.get_all(ExperienceLevel)
     return exp_levels
+
+@router.get("/work-types", response_model=List[LookupRead])
+async def get_work_types(lookups_service: Annotated[LookupsService, Depends(get_lookups_service)]):
+    work_types = await lookups_service.get_all(WorkType)
+    return work_types
