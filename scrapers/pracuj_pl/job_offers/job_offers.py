6326e1bef79394bfe93b27d93d995e8d05d06dbb
Kamil Kluska <kamil6kluska7@gmail.com>
2026-04-13 23:58:12 +0200

    feat: Extract offer details from next_data

--- scrapers/pracuj_pl/job_offers/job_offers.py
+++ scrapers/pracuj_pl/job_offers/job_offers.py
@@ -3,37 +3,57 @@ from typing import Dict, List, Any
 from pracuj_pl.helpers import extract_query
 from pracuj_pl.schemas import JobOffer
 
-QUERIES = {"jobOffers"}
 
-def extract_job_offers(next_data: Dict) -> JobOffer:
-    raw_lookups = extract_query(next_data, QUERIES)
+def extract_job_offers(next_data: Dict) -> List[JobOffer]:
+    raw_lookups = extract_query(next_data, {"jobOffers"})
 
     offers = raw_lookups.get("jobOffers", {}).get("groupedOffers")
 
-    offer: Dict[str, Any] = offers[0]
+    result = []
+    for offer in offers:
+        position_levels = offer.get("positionLevels", [])
+        work_modes = offer.get("workModes", [])
+        urls = offer.get("offers", [])
+        if urls and isinstance(urls[0], Dict):
+            url = urls[0].get("offerAbsoluteUri", "")
+        else:
+            url = ""
 
-    position_levels = offer.get("positionLevels", [])
-    work_modes = offer.get("workModes", [])
-    urls = offer.get("offers", [])
-    if urls and isinstance(urls[0], Dict):
-        url = urls[0].get("offerAbsoluteUri", "")
-        locations = []
-        for location in urls:
-            location_name = location.get("displayWorkplace", "").split(",")[0].strip()
-            locations.append(location_name)
-    else:
-        url = ""
-        locations = []
+        entity = JobOffer(
+            title=offer.get("jobTitle", "").strip(),
+            technologies=offer.get("technologies", []),
+            company=offer.get("companyName","").strip(),
+            exp_lvl=position_levels[0].strip() if position_levels else "",
+            work_type= work_modes[0].strip() if work_modes else "",
+            url=url.strip(),
+        )
+        result.append(entity)
 
-    entity = JobOffer(
-        title=offer.get("jobTitle", "").strip(),
-        technologies=offer.get("technologies", []),
-        company=offer.get("companyName","").strip(),
-        exp_lvl=position_levels[0].strip() if position_levels else "",
-        work_type= work_modes[0].strip() if work_modes else "",
-        url=url.strip(),
-        locations=locations
-    )
+    return result
 
-    return entity
+def fill_out_offer(next_data: Dict, job_offer: JobOffer) -> None:
+    raw_offer: Dict[str, Any] = extract_query(next_data, {"jobOffer"})
+    offer: Dict[str, Any] = raw_offer.get("jobOffer", {})
+    attributes: Dict[str, Any] = offer.get("attributes", {})
+    secondary_attributes: List[Dict[str, Any]] = offer.get("secondaryAttributes", [])
+    text_sections: Dict[str, Any] = offer.get("textSections", {})
+
+    # Locations
+    workplaces: List[Dict[str, Any]] = attributes.get("workplaces", [])
+    locations = []
+    for location in workplaces:
+        location_name: str = location.get("inlandLocation", {}).get("location", {}).get("name", "").strip()
+        locations.append(location_name)
+
+    # Specialization
+    spec_first_attr: Dict[str, Any] = secondary_attributes[0] if secondary_attributes else {}
+    spec_items = spec_first_attr.get("model", {}).get("items", [])
+    spec_details = spec_items[0] if spec_items else {}
+
+    # Description
+
+
+    job_offer.specialization = spec_details.get("name", "").strip()
+    job_offer.locations = locations
+    job_offer.description = "Brak"
 
