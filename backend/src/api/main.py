4f8b0c8b6cdd10a0d31db5ebf7da1c7e8078ff40
Ra1eB <piotrek.ba@op.pl>
2026-04-25 13:57:11 +0200

    Added CORS Middleware
    Added CORS for frontend-backend communication.

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.v1.router import router_v1
 from src.core.exceptions import RecordNotFoundError, RecordAlreadyExistsError, ValidationError
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_v1)
 
