181a5ff4226d084ab9b80b47db75755820f62860
Ra1eB <piotrek.ba@op.pl>
2026-04-26 22:17:39 +0200

    small KeyCloak change
    added frontend-client to realm json

--- frontend/src/app/keycloak.config.ts
+++ frontend/src/app/keycloak.config.ts
@@ -1,6 +1,5 @@
 export const keycloakConfig = {
   url: 'http://localhost:8080',
   realm: 'it-hell',
-  // clientId: 'frontend-client', // nie istnieje przynajmniej mi nie działa 
-  clientId: 'backend-client',
+  clientId: 'frontend-client'
 };
