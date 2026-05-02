3e31df1f64703df3be8d6462b857b66577d96b45
Kamil Kluska <kamil6kluska7@gmail.com>
2026-05-02 11:42:50 +0200

    fix(frontend): New title

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <header class="bg-white shadow">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">IT-Hell Job Board</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">IT-Hell</h1>
       </div>
     </header>
     <main>
