a3dacc1bfa2da14d0c4c044fb0e11cf11e295da1
Kamil Kluska <kamil6kluska7@gmail.com>
2026-05-13 22:10:23 +0200

    feat(scrapers): Add logger setup function

--- scrapers/main.py
+++ scrapers/main.py
@@ -4,33 +4,46 @@ import queue
 
 from logging.handlers import RotatingFileHandler, QueueHandler, QueueListener
 
-from pracuj_pl.scraper import ScraperPracujPL
+from src.pracuj_pl.scraper import ScraperPracujPL
+
+def setup_logger(
+        name: str,
+        max_bytes: int = 5*1024*1024,
+        backup_count: int = 3
+    ) -> logging.Logger:
+
+    log_que = queue.Queue()
+    que_handler = QueueHandler(log_que)
+
+    handler = RotatingFileHandler(
+        f'logs/{name}.log',
+        maxBytes=max_bytes,         # 5 MB per file
+        backupCount=backup_count    # Keep last 3 backups
+    )   
+    
+    listener = QueueListener(log_que, handler)
+    listener.start()
+    
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(que_handler)
+
+    return logger
 
 
 async def main():
 
     # Logger
-    log_que = queue.Queue()
-    que_handler = QueueHandler(log_que)
-    
-    handler = RotatingFileHandler(
-        'logs/Scrapers.log',
-        maxBytes=5*1024*1024,  # 5 MB per file
-        backupCount=3          # Keep last 3 backups
-    )
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-
-    listener = QueueListener(log_que, handler)
-    listener.start()
-
-    logger = logging.getLogger("TradeitScraper")
-    logger.setLevel(logging.INFO)
-    logger.addHandler(que_handler)
+    logger_pracuj_pl = setup_logger("PracujPL")
 
     # Pracuj.pl
-    scraper_pracuj_pl = ScraperPracujPL(logger)
+    scraper_pracuj_pl = ScraperPracujPL(logger_pracuj_pl)
     await scraper_pracuj_pl.run()
+
+    # JustJoin.it
 
 if __name__ == "__main__":
     asyncio.run(main())
