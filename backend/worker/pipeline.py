fd9527c7740a6e406dbbc4704dd03216e8b4c311
Kamil Kluska <kamil6kluska7@gmail.com>
2026-05-28 22:41:26 +0200

    feat(worker): Add more fields to normalize

--- backend/worker/pipeline.py
+++ backend/worker/pipeline.py
@@ -1,8 +1,16 @@
 from src.schemas.job_offers import JobOfferScraperCreate
 
-from worker.normalize.exp_lvls import normalize_experience
-from worker.normalize.technologies import normalize_technologies
+from worker.normalize import normalize_string, normalize_strings
+from worker.normalize.exp_lvls import EXPERIENCE_LEVELS
+from worker.normalize.work_types import WORK_TYPES
+from worker.normalize.technologies import TECHNOLOGIES
 
 def normalize_offer(offer: JobOfferScraperCreate) -> None:
-    offer.exp_level_name = normalize_experience(offer.exp_level_name)
-    offer.technology_names = normalize_technologies(offer.technology_names)
+    offer.title = offer.title.strip()
+    offer.company_name = offer.company_name.strip()
+    offer.work_type_name = normalize_string(offer.work_type_name, WORK_TYPES)
+    offer.exp_level_name = normalize_string(offer.exp_level_name, EXPERIENCE_LEVELS)
+    offer.technology_names = normalize_strings(offer.technology_names, TECHNOLOGIES)
+    offer.specialization_name = offer.specialization_name.strip()
+    offer.location_names = [city.strip().title() for city in offer.location_names]
+    offer.url = offer.url.strip()
